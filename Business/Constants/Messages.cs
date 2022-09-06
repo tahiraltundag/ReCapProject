e6950ad9052c726762002f9f31439951610a3424
tahir <tahiraltundag1@gmail.com>
2022-09-06 13:28:18 +0300

    6 eylül 2022

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,25 @@
+namespace Business.Constants;
+
+public class Messages
+{
+    public static string BrandListed = "Markalar listelendi";
+    public static string BrandAdded = "Marka eklendi";
+    public static string BrandUpdated = "Marka güncellendi";
+    public static string BrandDeleted = "Marka silindi";
+
+    public static string CarListed = "Arabalar listelendi";
+    public static string GetCarById = "Araba id sine göre listelendi";
+    public static string GetCarByBrandId = "Arabalar markalarına göre listelendi";
+    public static string GetCarByColorId = "Arabalar renklerine göre listelendi";
+    public static string CarAdded = "Araba eklendi";
+    public static string CarNotAdded = "Araba eklenemedi";
+    public static string CarUpdated = "Araba güncellendi";
+    public static string CarDeleted = "Araba silindi";
+
+    public static string ColorListed = "Renkler listelendi";
+    public static string GetByColorId = "Renkler id lerine göre listelendi";
+    public static string ColorAdded = "Renk eklendi";
+    public static string ColorDeleted = "Renk silindi";
+    public static string ColorUpdate = "Renk güncellendi";
+    
+}
---
+++ Core/Utilities/Results/DataResult.cs
@@ -0,0 +1,17 @@
+namespace Core.Utilities.Results;
+
+public class DataResult<T> : Result, IDataResult<T>
+{ 
+    T Data { get; }
+
+    public DataResult(T data, bool success, string message): base(success,message)
+    {
+        Data = data;
+    }
+
+    public DataResult(T data, bool success): base(success)
+    {
+        Data = data;
+    }
+    
+}
